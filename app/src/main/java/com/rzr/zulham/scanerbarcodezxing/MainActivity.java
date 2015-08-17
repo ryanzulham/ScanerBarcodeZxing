edd8b6622be41a49d4cb208e82971ac188864b49
Ryan Zulham Ramadhani <ryanzulham@gmail.com>
2015-08-17 18:16:24 +0700

    Upgrade code IntentResult

--- app/src/main/java/com/rzr/zulham/scanerbarcodezxing/MainActivity.java
+++ app/src/main/java/com/rzr/zulham/scanerbarcodezxing/MainActivity.java
@@ -1,30 +1,48 @@
 package com.rzr.zulham.scanerbarcodezxing;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener{
-Button btnScan;
+public class MainActivity extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnScan = (Button) findViewById(R.id.btnscan);
-        btnScan.setOnClickListener(this);
+    }
 
+    public void scanBarcode(View view){
+        new IntentIntegrator((Activity)this).initiateScan();
     }
 
     @Override
-    public void onClick(View v) {
-        IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
-        integrator.initiateScan();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
+
+
+
+
 }
