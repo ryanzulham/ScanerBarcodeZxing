b4f3b5c8ba9c6f8d9e820c235184df4593ade85c
Ryan Zulham Ramadhani <ryanzulham@gmail.com>
2015-08-18 00:11:17 +0700

    Create DatabaseHelper Using SQLite

--- app/src/main/java/com/rzr/zulham/scanerbarcodezxing/MainActivity.java
+++ app/src/main/java/com/rzr/zulham/scanerbarcodezxing/MainActivity.java
@@ -2,8 +2,8 @@ package com.rzr.zulham.scanerbarcodezxing;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,12 +11,13 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
 
     }
     //code ini untuk memanggil scanner dengan menggunakan button
@@ -33,6 +34,11 @@ public class MainActivity extends ActionBarActivity {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                boolean isInserted =  myDb.insertData(result.getContents().toString());
+                if (isInserted = true)
+                    Toast.makeText(this, "Code Berhasil Disimpan", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(this, "Code Gagal Disimpan", Toast.LENGTH_LONG).show();
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
